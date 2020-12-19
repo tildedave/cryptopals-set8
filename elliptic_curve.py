01d5ced686d06b6778a4d73c2db6870c0d550a64
Dave King <tildedave@gmail.com>
2020-12-19 10:15:44 -0800

    Begin problem 60 restructuring

--- elliptic_curve.py
+++ elliptic_curve.py
@@ -1,12 +1,31 @@
-from random import randint
 from typing import Tuple, Union
+from random import randint
+
 from numtheory import mod_divide, mod_sqrt
 
 
 EllipticCurvePoint = Tuple[int, int]
 
+"""
+This is a point at infinity.
+
+(It's assumed that (0, 1) isn't on the given curve.)
+"""
+EllipticCurveIdentity: EllipticCurvePoint = (0, 1)
+
 
 class EllipticCurve:
+    """
+    Abstract class for typing
+    """
+    def __contains__(self, p1: EllipticCurvePoint):
+        raise NotImplementedError
+
+    def scalar_mult(self, p1: EllipticCurvePoint, n: int):
+        raise NotImplementedError
+
+
+class WeierstrassCurve(EllipticCurve):
     """
     Curve defined by y^2 = x^3 + a*x + b
     """
@@ -18,7 +37,7 @@ class EllipticCurve:
         assert b % p != 1, '(0, 1) should not have been on curve'
 
     def __contains__(self, p1: EllipticCurvePoint):
-        if p1 == self.identity:
+        if p1 == EllipticCurveIdentity:
             return True
 
         x, y = p1
@@ -26,24 +45,35 @@ class EllipticCurve:
         rhs = (x * x * x + self.a * x + self.b) % self.prime
         return lhs == rhs
 
-    @property
-    def identity(self):
-        """
-        This is a point at infinity.
-
-        (It's assumed that (0, 1) isn't on the given curve.)
-        """
-        return (0, 1)
-
     def __str__(self):
         return (
             f'EllipticCurve y^2 = x^3 + {self.a}x + {self.b} '
             f'mod {self.prime}'
         )
 
+    def scalar_mult(self, p1: EllipticCurvePoint, n: int):
+        """
+        Return a point added to itself n times
+        """
+        y = EllipticCurveIdentity
+        if n == 0:
+            return y
+
+        if n < 0:
+            raise ValueError('Not implemented yet')
+
+        z = p1
+        while n > 0:
+            if n % 2 == 1:
+                y = add_point(z, y, self)
+            n = n // 2
+            z = add_point(z, z, self)
+
+        return y
+
 
 def invert_point(p1: EllipticCurvePoint,
-                 curve: EllipticCurve,
+                 curve: WeierstrassCurve,
                  ) -> EllipticCurvePoint:
     """
     Return the inverse of the given elliptic curve point
@@ -53,16 +83,16 @@ def invert_point(p1: EllipticCurvePoint,
 
 def add_point(p1: EllipticCurvePoint,
               p2: EllipticCurvePoint,
-              curve: EllipticCurve,
+              curve: WeierstrassCurve,
               ) -> EllipticCurvePoint:
-    if p1 == curve.identity:
+    if p1 == EllipticCurveIdentity:
         return p2
 
-    if p2 == curve.identity:
+    if p2 == EllipticCurveIdentity:
         return p1
 
     if p1 == invert_point(p2, curve):
-        return curve.identity
+        return EllipticCurveIdentity
 
     p = curve.prime
     x1, y1 = p1
@@ -78,28 +108,7 @@ def add_point(p1: EllipticCurvePoint,
     return (x3, y3)
 
 
-def scalar_mult_point(p1: EllipticCurvePoint, n: int, curve: EllipticCurve):
-    """
-    Return a point added to itself n times
-    """
-    y = curve.identity
-    if n == 0:
-        return y
-
-    if n < 0:
-        raise ValueError('Not implemented yet')
-
-    z = p1
-    while n > 0:
-        if n % 2 == 1:
-            y = add_point(z, y, curve)
-        n = n // 2
-        z = add_point(z, z, curve)
-
-    return y
-
-
-def find_point_on_curve(curve: EllipticCurve):
+def find_point_on_curve(curve: WeierstrassCurve):
     iterations = 0
     MAX_ITERATIONS = 5000
     while iterations < MAX_ITERATIONS:
@@ -119,13 +128,47 @@ def find_point_on_curve(curve: EllipticCurve):
     raise ValueError('Unable to find point on curve')
 
 
-def find_point_order(pt: EllipticCurvePoint, curve: EllipticCurve):
+def find_point_order(pt: EllipticCurvePoint, curve: WeierstrassCurve):
     """
     Debugging method to find the order of a given point.  Uses repeated adding
     """
     order = 1
-    while pt != curve.identity:
+    while pt != EllipticCurveIdentity:
         pt = add_point(pt, pt, curve)
         order += 1
 
     return order
+
+
+class MontgomeryCurve(EllipticCurve):
+    """
+    Curve defined by B*v^2 = u^3 + A*u^2 + u
+    """
+
+    def __init__(self, p, a, b):
+        self.prime = p
+        self.a = a % p
+        self.b = b % p
+        assert (0, 1) not in self, '(0, 1) should not have been on curve'
+
+    def __contains__(self, p1: EllipticCurvePoint):
+        # p1 is (u, v)
+        u, v = p1[0], p1[1]
+        p = self.prime
+
+        return (self.b * v * v) % p == (u * u * u + self.a * u * u + u) % p
+
+    def scalar_mult(self, p1: EllipticCurvePoint, n: int):
+        # Implement ladder
+        u, v = p1[0], p1[1]
+        p = self.prime
+
+        u2, w2 = (1, 0)
+        u3, w3 = (u, 1)
+
+
+
+def test_montgomery_contains():
+    p = 233970423115425145524320034830162017933
+    curve = MontgomeryCurve(p, 534, 1)
+    assert (4, 85518893674295321206118380980485522083) in curve
--- problem59.py
+++ problem59.py
@@ -4,24 +4,22 @@ from functools import reduce
 from operator import itemgetter, mul
 
 from elliptic_curve import (
-    EllipticCurve,
+    EllipticCurveIdentity,
     EllipticCurvePoint,
+    WeierstrassCurve,
     add_point,
     find_point_on_curve,
-    find_point_order,
-    invert_point,
-    scalar_mult_point,
 )
 from numtheory import small_factors, crt_inductive
 
 p = 233970423115425145524320034830162017933
-curve = EllipticCurve(p, -95051, 11279326)
+curve = WeierstrassCurve(p, -95051, 11279326)
 point = (182, 85518893674295321206118380980485522083)
 assert point in curve, 'Point was not on curve (somehow)'
 
 given_order = 29246302889428143187362802287225875743
-pt = scalar_mult_point(point, given_order, curve)
-assert pt == curve.identity, 'Point did not have expected order'
+pt = curve.scalar_mult(point, given_order)
+assert pt == EllipticCurveIdentity, 'Point did not have expected order'
 
 
 def generate_keypair() -> Tuple[int, EllipticCurvePoint]:
@@ -42,7 +40,7 @@ def generate_keypair() -> Tuple[int, EllipticCurvePoint]:
         (g^secret_A)^(secret_B) = (g^secret_B)^(secret_A)
     """
     secret = randint(0, given_order)
-    public = scalar_mult_point(point, secret, curve)
+    public = curve.scalar_mult(point, secret)
 
     return (secret, public)
 
@@ -52,23 +50,23 @@ def compute_secret(peer_public: EllipticCurvePoint,
     """
     Compute the public key ^ secret.
     """
-    return scalar_mult_point(peer_public, self_secret, curve)
+    return curve.scalar_mult(peer_public, self_secret)
 
 
-def find_point_of_order(r: int, curve_order: int, curve: EllipticCurve):
+def find_point_of_order(r: int, curve_order: int, curve: WeierstrassCurve):
     tries = 0
     while tries < 10:
         tries += 1
         pt = find_point_on_curve(curve)
-        candidate_point = scalar_mult_point(pt, curve_order // r, curve)
-        if candidate_point != curve.identity:
-            assert scalar_mult_point(candidate_point, r, curve) == curve.identity
+        candidate_point = curve.scalar_mult(pt, curve_order // r)
+        if candidate_point != EllipticCurveIdentity:
+            assert curve.scalar_mult(candidate_point, r) == EllipticCurveIdentity
             return candidate_point
 
     raise ValueError(f'Unable to find point of order {r} on curve {curve}')
 
 
-def subgroup_confinement_residues(curve: EllipticCurve,
+def subgroup_confinement_residues(curve: WeierstrassCurve,
                                   curve_order: int):
     """
     Return the residue of the secret key mod some value
@@ -92,7 +90,6 @@ def subgroup_confinement_residues(curve: EllipticCurve,
             continue
 
 
-
 if __name__ == "__main__":
     (alice_secret, alice_public) = generate_keypair()
     (bob_secret, bob_public) = generate_keypair()
@@ -103,11 +100,11 @@ if __name__ == "__main__":
     assert alice_key == bob_key, 'Key should have been shared'
 
     # Begin Subgroup Confinement Attack
-    bad_curve1 = EllipticCurve(p, -95051, 210)
+    bad_curve1 = WeierstrassCurve(p, -95051, 210)
     order_curve1 = 233970423115425145550826547352470124412
-    bad_curve2 = EllipticCurve(p, -95051, 504)
+    bad_curve2 = WeierstrassCurve(p, -95051, 504)
     order_curve2 = 233970423115425145544350131142039591210
-    bad_curve3 = EllipticCurve(p, -95051, 727)
+    bad_curve3 = WeierstrassCurve(p, -95051, 727)
     order_curve3 = 233970423115425145545378039958152057148
     bad_curves = [bad_curve1, bad_curve2, bad_curve3]
     curve_orders = [order_curve1, order_curve2, order_curve3]
