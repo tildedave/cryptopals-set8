8551e91654a899838924bfe29da81ad5801795b1
Dave King <tildedave@gmail.com>
2021-12-20 12:10:04 -0500

    Problem 64: Split context out of class

--- problem64.py
+++ problem64.py
@@ -1,4 +1,3 @@
-from math import log2
 from typing import Callable, List
 
 # Length = 128
@@ -61,65 +60,72 @@ class RingContext:
         self.element_add = element_add
         self.element_mult = element_mult
 
-    def matrix_multiply_naive(self, a: Matrix, b: Matrix) -> Matrix:
-        # Naive matrix multiplication.  Assumes a and b are square matrices of
-        # the same size.
-        # https://en.wikipedia.org/wiki/Matrix_multiplication_algorithm#Iterative_algorithm
-        c = make_matrix(size=len(a))
 
-        for i in range(0, len(a)):
-            for j in range(0, len(b)):
-                sum = 0
-                for n in range(0, len(a)):
-                    m = self.element_mult(a[i][n], b[n][j])
-                    sum = self.element_add(sum, m)
-                c[i][j] = sum
+def matrix_multiply_naive(ctx: RingContext, a: Matrix, b: Matrix) -> Matrix:
+    # Naive matrix multiplication.  Assumes a and b are square matrices of
+    # the same size.
+    # https://en.wikipedia.org/wiki/Matrix_multiplication_algorithm#Iterative_algorithm
+    c = make_matrix(size=len(a))
 
-        return c
+    for i in range(0, len(a)):
+        for j in range(0, len(b)):
+            sum = 0
+            for n in range(0, len(a)):
+                m = ctx.element_mult(a[i][n], b[n][j])
+                sum = ctx.element_add(sum, m)
+            c[i][j] = sum
 
-    def matrix_add(self, a: Matrix, b: Matrix) -> Matrix:
-        assert len(a) == len(b)
-        c = make_matrix(size=len(a))
-        for i in range(0, len(a)):
-            for j in range(0, len(a)):
-                c[i][j] = self.element_add(a[i][j], b[i][j])
+    return c
 
-        return c
 
-    def matrix_multiply_divide_and_conquer(self,
-                                           a: Matrix,
-                                           b: Matrix,
-                                           ) -> Matrix:
-        """
-        Multiply matrices via divide and conquer technique
+def matrix_add(ctx: RingContext, a: Matrix, b: Matrix) -> Matrix:
+    assert len(a) == len(b)
+    c = make_matrix(size=len(a))
+    for i in range(0, len(a)):
+        for j in range(0, len(a)):
+            c[i][j] = ctx.element_add(a[i][j], b[i][j])
 
-        Only works for matrices which are a power of 2 (can fix later if
-        needed)
-        """
-        a_size = len(a)
-        b_size = len(b)
-        assert a_size == b_size
+    return c
 
-        if a_size == 1:
-            return [[a[0][0] * b[0][0]]]
 
-        a_11, a_12, a_21, a_22 = split_square_matrix(a)
-        b_11, b_12, b_21, b_22 = split_square_matrix(b)
+def matrix_multiply_divide_and_conquer(ctx: RingContext,
+                                       a: Matrix,
+                                       b: Matrix,
+                                       ) -> Matrix:
+    """
+    Multiply matrices via divide and conquer technique
 
-        c_11 = self.matrix_add(
-            self.matrix_multiply_divide_and_conquer(a_11, b_11),
-            self.matrix_multiply_divide_and_conquer(a_12, b_21))
-        c_12 = self.matrix_add(
-            self.matrix_multiply_divide_and_conquer(a_11, b_12),
-            self.matrix_multiply_divide_and_conquer(a_12, b_22))
-        c_21 = self.matrix_add(
-            self.matrix_multiply_divide_and_conquer(a_21, b_11),
-            self.matrix_multiply_divide_and_conquer(a_22, b_21))
-        c_22 = self.matrix_add(
-            self.matrix_multiply_divide_and_conquer(a_21, b_12),
-            self.matrix_multiply_divide_and_conquer(a_22, b_22))
+    Only works for matrices which are a power of 2 (can fix later if
+    needed)
+    """
+    a_size = len(a)
+    b_size = len(b)
+    assert a_size == b_size
 
-        return combine_matrices(c_11, c_12, c_21, c_22)
+    if a_size == 1:
+        return [[a[0][0] * b[0][0]]]
+
+    a_11, a_12, a_21, a_22 = split_square_matrix(a)
+    b_11, b_12, b_21, b_22 = split_square_matrix(b)
+
+    c_11 = matrix_add(
+        ctx,
+        matrix_multiply_divide_and_conquer(ctx, a_11, b_11),
+        matrix_multiply_divide_and_conquer(ctx, a_12, b_21))
+    c_12 = matrix_add(
+        ctx,
+        matrix_multiply_divide_and_conquer(ctx, a_11, b_12),
+        matrix_multiply_divide_and_conquer(ctx, a_12, b_22))
+    c_21 = matrix_add(
+        ctx,
+        matrix_multiply_divide_and_conquer(ctx, a_21, b_11),
+        matrix_multiply_divide_and_conquer(ctx, a_22, b_21))
+    c_22 = matrix_add(
+        ctx,
+        matrix_multiply_divide_and_conquer(ctx, a_21, b_12),
+        matrix_multiply_divide_and_conquer(ctx, a_22, b_22))
+
+    return combine_matrices(c_11, c_12, c_21, c_22)
 
 
 IntegerContext = RingContext(lambda x, y: x + y, lambda x, y: x * y)
@@ -130,12 +136,12 @@ def test_matrix_multiply():
     a = [[0, 1], [1, 0]]
     b = [[2, 3], [3, 4]]
 
-    assert IntegerContext.matrix_multiply_naive(b, a) == [[3, 2], [4, 3]]
-    assert IntegerContext.matrix_multiply_divide_and_conquer(b, a) == \
+    assert matrix_multiply_naive(IntegerContext, b, a) == [[3, 2], [4, 3]]
+    assert matrix_multiply_divide_and_conquer(IntegerContext, b, a) == \
         [[3, 2], [4, 3]]
 
     c = [[2, 7, 3], [1, 5, 8], [0, 4, 1]]
     d = [[3, 0, 1], [2, 1, 0], [1, 2, 4]]
 
-    assert IntegerContext.matrix_multiply_naive(c, d) == \
+    assert matrix_multiply_naive(IntegerContext, c, d) == \
         [[23, 13, 14], [21, 21, 33], [9, 6, 4]]
